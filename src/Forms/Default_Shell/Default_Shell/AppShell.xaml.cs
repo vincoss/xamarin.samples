1af49b62bc031f1c16f727d2e407b8a5e7fd9a66
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-17 07:50:57 +1000

    fix projects build path too long, git ignore update

---
+++ src/Forms/Default_Shell/Default_Shell/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Default_Shell
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Forms/Default_Shell/Default_Shell/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Default_Shell.Models;
+
+namespace Default_Shell.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
