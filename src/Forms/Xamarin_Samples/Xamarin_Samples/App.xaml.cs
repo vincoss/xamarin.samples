5281274dc9d43fcf7aac7fc9a5e12e637621142b
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-21 15:24:53 +1000

    images samples

---
+++ src/Forms/Xamarin_Samples/Xamarin_Samples/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin_Samples.Views;
+
+namespace Xamarin_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ImageSamplesView();
+        }
+
+        protected override void OnStart()
+        {
+            ListAllEmbeddedResourceNamesDevOnly();
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+        /// <summary>
+        /// Troubleshooting Embedded Images
+        /// </summary>
+        private void ListAllEmbeddedResourceNamesDevOnly()
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+            {
+                System.Diagnostics.Debug.WriteLine($"Found resource: {res}");
+            }
+        }
+    }
+}
