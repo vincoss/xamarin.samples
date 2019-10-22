23f957c937249f678305e923eb09d9aaf2955597
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-23 08:05:07 +1000

    localiztion sample

---
+++ src/Forms/Xamarin_Localization/Xamarin_Localization/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin_Localization.Resources;
+
+namespace Xamarin_Localization
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Sample();
+        }
+
+        public void Sample()
+        {
+            AppResources.Culture = new CultureInfo("sk-SK");
+
+            // Using Resources in Code
+            var myLabel = new Label();
+            var myEntry = new Entry();
+            // populate UI with translated text values from resources
+            myLabel.Text = AppResources.WeekDayKey;
+            myEntry.Placeholder = AppResources.WeekDayPlaceholderKey;
+
+            localizationSample.Children.Add(myLabel);
+            localizationSample.Children.Add(myEntry);
+        }
+    }
+}
