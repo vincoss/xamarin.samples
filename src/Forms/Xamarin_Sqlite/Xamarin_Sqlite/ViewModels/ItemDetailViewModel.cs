11e577351427ec5ac626d3fc086b3ed86914eafe
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-25 08:50:54 +1000

    Sqlite sample

---
+++ src/Forms/Xamarin_Sqlite/Xamarin_Sqlite/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin_Sqlite.Models;
+
+namespace Xamarin_Sqlite.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
