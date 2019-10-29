86440a749ce8a4552393fbe3dad02f8dbff96792
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-29 14:30:29 +1000

    SearchBox sample

--- src/Forms/Xamarin_Samples/Xamarin_Samples/App.xaml.cs
+++ src/Forms/Xamarin_Samples/Xamarin_Samples/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Xamarin_Samples
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new SendEmailSampleView());
+            MainPage = new NavigationPage(new SearchBarView());
         }
 
         protected override void OnStart()
