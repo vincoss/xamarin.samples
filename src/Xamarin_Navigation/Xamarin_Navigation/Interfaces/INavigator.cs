f72cf79b92f1c942f98bd47a8eff3a125a6d0660
Ferdinand Lukasak <feroalien@hotmail.com>
2019-10-29 13:42:58 +1000

    Navigation upate

--- src/Xamarin_Navigation/Xamarin_Navigation/Interfaces/INavigator.cs
+++ src/Xamarin_Navigation/Xamarin_Navigation/Interfaces/INavigator.cs
@@ -5,6 +5,10 @@ namespace Xamarin_Navigation.Interfaces
 {
     public interface INavigator
     {
+        Task PushAsync(Type pageType, object model);
+
         Task PushModalAsync(Type pageType, object model);
+
+        Task InsertPageBefore(Type pageType, Type beforePageType, object model);
     }
 }
--- src/Xamarin_Navigation/Xamarin_Navigation/Services/Navigator.cs
+++ src/Xamarin_Navigation/Xamarin_Navigation/Services/Navigator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin_Navigation.Interfaces;
@@ -10,7 +11,46 @@ namespace Xamarin_Navigation.Services
 {
     public class Navigator : INavigator
     {
-        public async Task PushModalAsync(Type pageType, object model)
+        public Task InsertPageBefore(Type pageType, Type beforePageType, object model)
+        {
+            if (pageType == null)
+            {
+                throw new ArgumentNullException(nameof(pageType));
+            }
+            if (beforePageType == null)
+            {
+                throw new ArgumentNullException(nameof(beforePageType));
+            }
+            var page = Get(pageType);
+            if (page != null && model != null)
+            {
+                page.BindingContext = model;
+            }
+            var previous = Application.Current.MainPage.Navigation.NavigationStack.SingleOrDefault(x => x.GetType() == beforePageType);
+            if(previous == null)
+            {
+                throw new InvalidOperationException(beforePageType.FullName);
+            }
+            Application.Current.MainPage.Navigation.InsertPageBefore(page, previous);
+            Application.Current.MainPage.Navigation.RemovePage(previous);
+            return Task.CompletedTask;
+        }
+
+        public Task PushAsync(Type pageType, object model)
+        {
+            if (pageType == null)
+            {
+                throw new ArgumentNullException(nameof(pageType));
+            }
+            var page = Get(pageType);
+            if (page != null && model != null)
+            {
+                page.BindingContext = model;
+            }
+            return Application.Current.MainPage.Navigation.PushAsync(page);
+        }
+
+        public  Task PushModalAsync(Type pageType, object model)
         {
             if(pageType == null)
             {
@@ -21,7 +61,7 @@ namespace Xamarin_Navigation.Services
             {
                 page.BindingContext = model;
             }
-            await Application.Current.MainPage.Navigation.PushModalAsync(page);
+           return  Application.Current.MainPage.Navigation.PushModalAsync(page);
         }
 
         private Page Get(Type type)
