5ae60c781f768bf86c479bae250aedb3f97a6486
Ferdinand Lukasak <feroalien@hotmail.com>
2019-11-05 16:34:34 +1000

    validation sample

---
+++ src/Forms/Xamarin_Validation/Xamarin_Validation/Validation/GuidValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_Validation.Validation
+{
+    public class GuidValidator : AbstractValidator<string>
+    {
+        public GuidValidator(string propertyName)
+        {
+            RuleFor(x => x).NotEmpty().OverridePropertyName(propertyName);
+        }
+    }
+}
