116676849b02ecbb892ca556491001f2da28623d
fero <feroalien@hotmail.com>
2019-11-15 15:46:12 +1000

    android splash sample

---
+++ src/Forms/Xamarin_SplashSample/Xamarin_SplashSample.Android/Activities/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Xamarin_SplashSample.Droid.Activities
+{
+    [Activity(
+          Label = "@string/ApplicationName",
+          Icon = "@mipmap/icon",
+          Theme = "@style/Theme.Splash",
+          NoHistory = true,
+          MainLauncher = true,
+          ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            InvokeMainActivity();
+        }
+
+        private void InvokeMainActivity()
+        {
+            StartActivity(new Intent(this, typeof(MainActivity)));
+        }
+    }
+}
