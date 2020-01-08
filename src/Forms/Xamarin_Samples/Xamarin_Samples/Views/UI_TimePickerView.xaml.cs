6c5941e6a69482efec83c0e26c0f643ed7db0e93
fero <feroalien@hotmail.com>
2020-01-08 14:33:01 +1000

    set values samples

---
+++ src/Forms/Xamarin_Samples/Xamarin_Samples/Views/UI_TimePickerView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin_Samples.ViewModels;
+
+namespace Xamarin_Samples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UI_TimePickerView : ContentPage
+    {
+        public UI_TimePickerView()
+        {
+            InitializeComponent();
+
+            var model = new UI_TimePickerViewModel();
+            BindingContext = model;
+        }
+    }
+
+    public class UI_TimePickerViewModel : BaseViewModel
+    {
+        DateTime _selectedTime = DateTime.Now;
+        public DateTime SelectedTime
+        {
+            get { return _selectedTime; }
+            set { SetProperty(ref _selectedTime, value); }
+        }
+    }
+}
