5648f256c802acd6f0c93e8c6fc1525039bb7f26
fero <feroalien@hotmail.com>
2020-02-10 12:14:31 +1000

    preferences sample, optional page to start

---
+++ src/Forms/Xamarin_Samples_Preferences/Xamarin_Samples_Preferences/Views/OneView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Samples_OptionalStartPage.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OneView : ContentPage
+    {
+        public OneView()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HomeView());
+        }
+    }
+}
