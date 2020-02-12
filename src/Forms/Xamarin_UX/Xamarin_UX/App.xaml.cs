a0f7c4e3bb704cdda4caa04a76b7f6d126cd7750
fero <feroalien@hotmail.com>
2020-02-12 16:54:41 +1000

    validation template

---
+++ src/Forms/Xamarin_UX/Xamarin_UX/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin_UX.Views;
+
+namespace Xamarin_UX
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new ValidationView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
