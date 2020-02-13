c873f47f7f7f4de93049480a380c92b2b075515c
fero <feroalien@hotmail.com>
2020-02-13 10:18:37 +1000

    validation template and label behavior

---
+++ src/Forms/Xamarin_UX/Xamarin_UX/Xaml/LabelVisibilityBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Linq;
+using System.ComponentModel;
+
+
+namespace Xamarin_Samples.Xaml
+{
+    public class LabelVisibilityBehavior : Behavior<Label>
+    {
+        protected override void OnAttachedTo(Label entry)
+        {
+            entry.IsVisible = false;
+            entry.PropertyChanged += Entry_PropertyChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Label entry)
+        {
+            entry.IsVisible = false;
+            entry.PropertyChanged -= Entry_PropertyChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void Entry_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var label = (Label)sender;
+            if (label == null)
+            {
+                return;
+            }
+
+            if (e.PropertyName == nameof(Label.Text))
+            {
+                label.IsVisible = false;
+
+                if (string.IsNullOrWhiteSpace(label.Text) == false)
+                {
+                    label.IsVisible = true;
+                }
+            }
+        }
+
+    }
+}
