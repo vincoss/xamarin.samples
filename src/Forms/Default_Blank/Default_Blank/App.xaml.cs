4469ed24c249857cf5a25a0df8493f443e5c1eb1
fero <feroalien@hotmail.com>
2020-04-22 10:03:30 +1000

    default project update

--- src/Forms/Default_Blank/Default_Blank/App.xaml.cs
+++ src/Forms/Default_Blank/Default_Blank/App.xaml.cs
@@ -15,17 +15,14 @@ namespace Default_Blank
 
         protected override void OnStart()
         {
-            // Handle when your app starts
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
         }
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
         }
     }
 }
