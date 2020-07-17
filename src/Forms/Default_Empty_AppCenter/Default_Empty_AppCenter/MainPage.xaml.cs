44da05420d4ba39c9986cd9505358473d713759a
fero <feroalien@hotmail.com>
2020-07-17 14:05:03 +1000

    appcenter sample

---
+++ src/Forms/Default_Empty_AppCenter/Default_Empty_AppCenter/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Default_Empty_AppCenter
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestCrash_Clicked(object sender, EventArgs e)
+        {
+            Crashes.GenerateTestCrash();
+        }
+
+        private void btnExceptionBasic_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var x = 0;
+                var a = 2 / x;
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+            }
+        }
+
+        private void btnExceptionWithData_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var x = 0;
+                var a = 2 / x;
+            }
+            catch (Exception exception)
+            {
+                var properties = new Dictionary<string, string> {
+    { "Category", "Music" },
+    { "Wifi", "On" }
+  };
+                Crashes.TrackError(exception, properties);
+            }
+        }
+    }
+}
