9fa0009c37cd85edd371b716b923735d6c5884b2
fero <feroalien@hotmail.com>
2020-09-09 16:44:41 +1000

    Xamarin.OAuth sample

---
+++ src/Forms/Xamarin_Auth_Samples/Xamarin_Auth_Samples/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Auth;
+using Xamarin.Auth.Presenters;
+using Xamarin.Forms;
+
+namespace Xamarin_Auth_Samples
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMicrosoft_Clicked(object sender, EventArgs e)
+        {
+            var clientId = "TODO - client ID";
+            var scope = "User.Read";
+            var tenant = "TODO - tenant";
+            var redirectUrl = "com.companyname.webauthenticatorsamples://oauth2redirect";
+
+            var auth = new OAuth2Authenticator(
+            clientId: clientId,
+            scope: scope,
+            authorizeUrl: new Uri($"https://login.microsoftonline.com/{tenant}/oauth2/authorize"),
+            redirectUrl: new Uri(redirectUrl),
+            isUsingNativeUI: false);
+
+            auth.Completed += Authenticator_Completed;
+            auth.Error += Authenticator_Error;
+
+            // NOTE: Null reference thrown if missing
+            // Add this into App.cs  global::Xamarin.Auth.Presenters.UWP.AuthenticationConfiguration.Init();
+            var presenter = new OAuthLoginPresenter();
+            presenter.Login(auth);
+        }
+
+        private void Authenticator_Error(object sender, AuthenticatorErrorEventArgs e)
+        {
+            lblInfo.Text = e.Message;
+        }
+
+        private void Authenticator_Completed(object sender, AuthenticatorCompletedEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ src/Forms/Xamarin_Auth_Samples/Xamarin_Auth_Samples/Readme.txt
@@ -0,0 +1,3 @@
+﻿## References
+https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow
+https://docs.microsoft.com/en-us/xamarin/essentials/web-authenticator?tabs=ios#ios
