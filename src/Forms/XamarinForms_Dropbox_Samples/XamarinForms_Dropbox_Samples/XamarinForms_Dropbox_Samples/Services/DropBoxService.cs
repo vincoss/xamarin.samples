8cf170a2ac850aebda95122191f5227957b6676d
fero <feroalien@hotmail.com>
2020-10-01 09:51:03 +1000

    added Dropbox project sample

---
+++ src/Forms/XamarinForms_Dropbox_Samples/XamarinForms_Dropbox_Samples/XamarinForms_Dropbox_Samples/Services/DropBoxService.cs
@@ -0,0 +1,19 @@
+﻿using Dropbox.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinForms_Dropbox_Samples.Services
+{
+    public class DropBoxService
+    {
+        public void Sample()
+        {
+            using (var dbx = new DropboxClient("YOUR ACCESS TOKEN"))
+            {
+            }
+
+            
+        }
+    }
+}
