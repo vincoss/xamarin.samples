14321b770e450390befc14309475a2d6afbded4c
fero <feroalien@hotmail.com>
2020-12-03 17:28:52 +1000

    custom toast message

--- src/Forms/ToastMessage_Sample/ToastMessage_Sample/MainPage.xaml.cs
+++ src/Forms/ToastMessage_Sample/ToastMessage_Sample/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ToastMessage_Sample.Interface;
@@ -11,9 +12,13 @@ namespace ToastMessage_Sample
 {
     public partial class MainPage : ContentPage
     {
+        private MainPageViewModel _model = new MainPageViewModel();
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = _model;
         }
 
         private void Button_ClickedShort(object sender, EventArgs e)
@@ -27,5 +32,36 @@ namespace ToastMessage_Sample
             var service = DependencyService.Get<IMessage>();
             service.LongAlert("I'm Long Alert");
         }
+
+        private void Button_ClickedCustom(object sender, EventArgs e)
+        {
+            _model.ToastMessage = "I'm custom alert.";
+        }
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private string _toastMessage;
+        public string ToastMessage
+        {
+            get { return _toastMessage; }
+            set
+            {
+                _toastMessage = value;
+                OnPropertyChanged(nameof(ToastMessage));
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
---
+++ src/Forms/ToastMessage_Sample/ToastMessage_Sample/Views/ToastView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToastMessage_Sample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToastView : ContentView
+    {
+        public ToastView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ToastMessageProperty = BindableProperty.Create("ToastMessage", typeof(string), typeof(ToastView), null, propertyChanged: OnToastMessageChanged);
+
+        public string ToastMessage
+        {
+            get { return (string)GetValue(ToastMessageProperty); }
+            set { SetValue(ToastMessageProperty, value); }
+        }
+
+        private async static void OnToastMessageChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (newValue == null)
+            {
+                return;
+            }
+
+            var value = (string)newValue;
+            var view = (ToastView)bindable;
+
+            view.toastMessage.Text = value;
+
+            // Fade in
+            await view.FadeTo(1, 0);
+            view.IsVisible = true;
+
+            // Fede out
+            await view.FadeTo(0, 1500);
+            view.IsVisible = false;
+
+            view.ToastMessage = null;
+        }
+    }
+
+    public class ToastViewModel
+    {
+        public bool IsRunning { get; set; }
+        public string Text { get; set; }
+    }
+}
