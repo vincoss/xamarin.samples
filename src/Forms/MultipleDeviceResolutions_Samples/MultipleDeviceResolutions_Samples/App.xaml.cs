e9c922a5a7973092ef0adffcc3b0321fa2809233
fero <feroalien@hotmail.com>
2021-01-15 15:42:36 +1000

    multiple resolutions sample initial

---
+++ src/Forms/MultipleDeviceResolutions_Samples/MultipleDeviceResolutions_Samples/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MultipleDeviceResolutions_Samples.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace MultipleDeviceResolutions_Samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HomeView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
