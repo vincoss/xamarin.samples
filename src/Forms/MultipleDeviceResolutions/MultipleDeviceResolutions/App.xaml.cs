b023e234311bab1b0fcc367b9f3cb37dca16b3e3
fero <feroalien@hotmail.com>
2021-01-24 12:53:50 +1000

    project reference update

--- src/Forms/MultipleDeviceResolutions/MultipleDeviceResolutions/App.xaml.cs
+++ src/Forms/MultipleDeviceResolutions/MultipleDeviceResolutions/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MultipleDeviceResolutions.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MultipleDeviceResolutions
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomeView();
         }
 
         protected override void OnStart()
--- src/Forms/MultipleDeviceResolutions/MultipleDeviceResolutions/Views/HomeView.xaml.cs
+++ src/Forms/MultipleDeviceResolutions/MultipleDeviceResolutions/Views/HomeView.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace MultipleDeviceResolutions
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MultipleDeviceResolutions.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeView : ContentPage
     {
-        public MainPage()
+        public HomeView()
         {
             InitializeComponent();
         }
