ea5c9d523d08188d4d7de0c896bf976babfddded
fero <feroalien@hotmail.com>
2021-01-25 08:05:13 +1000

    sample control

--- src/Forms/SignaturePadSample/SignaturePadSample/App.xaml.cs
+++ src/Forms/SignaturePadSample/SignaturePadSample/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SignaturePadSample.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace SignaturePadSample
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomeView();
         }
 
         protected override void OnStart()
--- src/Forms/SignaturePadSample/SignaturePadSample/Views/HomeView.xaml.cs
+++ src/Forms/SignaturePadSample/SignaturePadSample/Views/HomeView.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace SignaturePadSample
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SignaturePadSample.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeView : ContentPage
     {
-        public MainPage()
+        public HomeView()
         {
             InitializeComponent();
         }
