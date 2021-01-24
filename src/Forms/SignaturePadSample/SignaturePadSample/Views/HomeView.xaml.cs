2411eace4ba7d2e51397a9309a5392a3e8606d6d
fero <feroalien@hotmail.com>
2021-01-25 08:15:32 +1000

    signature sample

--- src/Forms/SignaturePadSample/SignaturePadSample/Views/HomeView.xaml.cs
+++ src/Forms/SignaturePadSample/SignaturePadSample/Views/HomeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SignaturePad.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,37 @@ namespace SignaturePadSample.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomeView : ContentPage
     {
+        private IEnumerable<IEnumerable<Point>> _signatureStrokes;
+
         public HomeView()
         {
             InitializeComponent();
         }
+
+        private void Init()
+        {
+            entryAppPath.Text = GetAppRootPath();
+        }
+
+        private async void btnSave_Clicked(object sender, EventArgs e)
+        {
+            var bitmap = await signatureView.GetImageStreamAsync(SignatureImageFormat.Png);
+        }
+
+        private void btnGetPoints_Clicked(object sender, EventArgs e)
+        {
+            _signatureStrokes = signatureView.Strokes;
+        }
+
+        private void btnRestore_Clicked(object sender, EventArgs e)
+        {
+           // signatureView.Strokes = newStrokes;
+        }
+
+        public static string GetAppRootPath()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        }
+
     }
 }
