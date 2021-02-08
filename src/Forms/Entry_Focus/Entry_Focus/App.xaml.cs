723c88c0c026194080608f82257136a1c9baf5e5
fero <feroalien@hotmail.com>
2021-02-08 16:37:44 +1000

    build fix

--- src/Forms/Entry_Focus/Entry_Focus/App.xaml.cs
+++ src/Forms/Entry_Focus/Entry_Focus/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Entry_Focus
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
