1685f7edc42a9365090eaf470b6d826d679d2c2c
fero <feroalien@hotmail.com>
2021-02-15 11:48:09 +1000

    icons iOS

--- src/Forms/CompleteAppWithIcons/CompleteAppWithIcons.Android/MainActivity.cs
+++ src/Forms/CompleteAppWithIcons/CompleteAppWithIcons.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CompleteAppWithIcons.Droid
 {
-    [Activity(Label = "CompleteAppWithIcons", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CompleteAppWithIcons", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- src/Forms/CompleteAppWithIcons/CompleteAppWithIcons/Readme.txt
+++ src/Forms/CompleteAppWithIcons/CompleteAppWithIcons/Readme.txt
@@ -5,3 +5,8 @@ Sign certificate
 
 # Android
 resize the icon with https://romannurik.github.io/AndroidAssetStudio/index.html
+
+# iOS
+https://docs.microsoft.com/en-us/xamarin/ios/app-fundamentals/images-icons/app-icons?tabs=windows
+https://appicon.co/
+https://www.iconcutter.com/
