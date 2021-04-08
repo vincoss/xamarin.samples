89de1fbb008636b714a80fd8adbb6693a1c30614
fero <feroalien@hotmail.com>
2021-04-08 18:17:35 +1000

    signature update

--- src/Forms/SignaturePadSample/SignaturePadSample/Xaml/SignaturePadViewAttached.cs
+++ src/Forms/SignaturePadSample/SignaturePadSample/Xaml/SignaturePadViewAttached.cs
@@ -1,11 +1,20 @@
 ﻿using SignaturePad.Forms;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace SignaturePadSample.Xaml
 {
+    /*
+        push strokes
+        get strokes
+        get stream
+    */
+
     public class SignaturePadViewAttached
     {
         public static BindableProperty StrokesProperty =
@@ -29,14 +38,74 @@ namespace SignaturePadSample.Xaml
             {
                 return;
             }
+            var points = control.Strokes;
+            //control.TextChanged += (sender, e) =>
+            //{
+            //    var s = sender as SearchBar;
+            //    var command = GetTextChangedCommand(s);
 
-            control.TextChanged += (sender, e) =>
-            {
-                var s = sender as SearchBar;
-                var command = GetTextChangedCommand(s);
-
-                command.Execute(e.NewTextValue);
-            };
+            //    command.Execute(e.NewTextValue);
+            //};
         }
     }
+
+    public interface ISignaturePadView
+    {
+       // string StrokesJson { get; set; }
+        Task<Stream> GetSignature();
+    }
+
+    public class CustomSignaturePadView : SignaturePadView, ISignaturePadView
+    {
+        public static BindableProperty StrokesJsonProperty = BindableProperty.Create(
+                nameof(StrokesJson),
+                typeof(string),
+                typeof(CustomSignaturePadView),
+                null,
+                propertyChanged: (bindable, oldValue, newValue) => ((CustomSignaturePadView)bindable).SignaturePadCanvas.Strokes = ParseStrokes((string)newValue));
+
+        private static IEnumerable<IEnumerable<Point>> ParseStrokes(string strokesJson)
+        {
+            if(string.IsNullOrWhiteSpace(strokesJson))
+            {
+                return null;
+            }
+            return JsonSerializer.Deserialize<IEnumerable<IEnumerable<Point>>>(strokesJson);
+        }
+
+
+
+        /// <summary>
+        /// Gets or sets the strokes.
+        /// </summary>
+        public string StrokesJson
+        {
+            get => (string)GetValue(StrokesJsonProperty);
+            set => SetValue(StrokesJsonProperty, value);
+        }
+
+        static void HandleChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = bindable as SignaturePadView;
+
+            if (control == null)
+            {
+                return;
+            }
+            var points = control.Strokes;
+            //control.TextChanged += (sender, e) =>
+            //{
+            //    var s = sender as SearchBar;
+            //    var command = GetTextChangedCommand(s);
+
+            //    command.Execute(e.NewTextValue);
+            //};
+        }
+
+        public async Task<Stream> GetSignature()
+        {
+            return await GetImageStreamAsync(SignatureImageFormat.Png);
+        }
+    }
+
 }
