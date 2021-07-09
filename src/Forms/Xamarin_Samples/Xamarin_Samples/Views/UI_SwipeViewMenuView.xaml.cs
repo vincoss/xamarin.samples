d61949e5cc6c4ec2767d348933ac8514ac819601
fero <feroalien@hotmail.com>
2021-07-09 13:26:09 +1000

    swipeView menu sample

---
+++ src/Forms/Xamarin_Samples/Xamarin_Samples/Views/UI_SwipeViewMenuView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Samples.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UI_SwipeViewMenuView : ContentPage
+    {
+        public UI_SwipeViewMenuView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            swipeView.Open(OpenSwipeItem.LeftItems);
+        }
+    }
+}
